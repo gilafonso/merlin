1a2d752371b952e2e30a2005fc0f84591541a120
Gil Afonso <gil.lomba.afonso@gmail.com>
2020-10-04 10:57:01 +0100

    add .env file

--- Merlinwebsite/settings.py
+++ Merlinwebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8_1qfsm2ebt+l94&e2lq&19f8b(vt%df4%pl9lim(nej$d)*er'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
